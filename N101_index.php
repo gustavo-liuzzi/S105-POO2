24566911ddd4eb05c50a81f1f0d62944b86637c6
Gustavo A. Liuzzi <gustavo.liuzzi@gmail.com>
2025-05-21 11:07:41 +0200

    Elimino código comentado

--- N101_index.php
+++ N101_index.php
@@ -4,8 +4,6 @@ include "classes\N101_Animal.php";
 include "classes\N101_Perro.php";
 include "classes\N101_Gato.php";
 
-/* Prueba de clases y método makeSound()*/
-
 $gatoUno = new Gato("Pedro");
 $perroUno = new Perro("Jacinto");
 echo $gatoUno->getNombre()." dice: ";
